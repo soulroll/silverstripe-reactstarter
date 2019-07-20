eb337d00e93492a5e91e75fe63f55a9830964519
David Malone <david@souldesign.net.nz>
2019-07-20 21:32:05 +1200

    MINOR: Working compile

--- themes/reactstarter/src/js/App.js
+++ themes/reactstarter/src/js/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 
-import Header from './components/header/header';
-import Main from './components/main/main';
-import Footer from './components/footer/footer';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main />
-        <Footer />
       </div>
     );
   }
