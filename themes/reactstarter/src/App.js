f288991c46994398eb232ee91df82087c84bb195
David Malone <david@souldesign.net.nz>
2019-07-21 20:01:39 +1200

    MINOR: Dev server installed but not working

--- themes/reactstarter/src/App.js
+++ themes/reactstarter/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <p>React here!</p>
+        <p>Reacts here!</p>
         <Footer />
       </div>
     );
