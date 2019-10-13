33fd65fe013ab534f99cc472e188bdc8ec93b037
David Malone <davidm@catalyst.net.nz>
2019-10-14 12:58:10 +1300

    MINOR: fixed some code decrapancy

--- themes/reactstarter/src/components/carousel/carousel.jsx
+++ themes/reactstarter/src/components/carousel/carousel.jsx
@@ -2,30 +2,24 @@ import React, {Component} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import './carousel.scss';
 
-class BootstrapCarousel extends Component {
-  render() {
+const BootstrapCarousel = (props) =>  {
 
+  //console.log(props.items);
+  const {data} = props;
 
-    console.log(this.props.items);
-
-    const {data} = this.props;
-
-    return (
-      <div>
-        <Carousel>
-        {this.props.items.map(item => (
-          <Carousel.Item key={item.node.ID}>
-            <img
-              className="d-block w-100"
-              src={item.node.getImageLink}
-              alt={item.node.Title}
-            />
-          </Carousel.Item>
-        ))}
-        </Carousel>
-      </div>
-    );
-  }
+  return (
+    <Carousel>
+      {props.items.map(item => (
+        <Carousel.Item key={item.node.ID}>
+          <img
+            className="d-block w-100"
+            src={item.node.getImageLink}
+            alt={item.node.Title}
+          />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  )
 }
 
 export default BootstrapCarousel;
