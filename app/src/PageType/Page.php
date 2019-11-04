76bc55bc34f82ef3994f28bf88944a30aaefb4b8
David Malone <davidm@catalyst.net.nz>
2019-11-04 16:11:30 +1300

    MAJOR: working sitetree query

--- app/src/PageType/Page.php
+++ app/src/PageType/Page.php
@@ -2,12 +2,46 @@
 
 namespace {
 
-    use SilverStripe\CMS\Model\SiteTree;
+	use SilverStripe\CMS\Model\SiteTree;
+	use SilverStripe\ORM\DataObject;
+	use SilverStripe\GraphQL\Scaffolding\Interfaces\ScaffoldingProvider;
+	use SilverStripe\GraphQL\Scaffolding\Scaffolders\SchemaScaffolder;
 
-    class Page extends SiteTree
-    {
-        private static $db = [];
+	class Page extends SiteTree implements ScaffoldingProvider
+	{
+		private static $db = [];
 
-        private static $has_one = [];
-    }
+		private static $has_one = [];
+
+		/**
+		 * @param SchemaScaffolder $scaffolder Scaffolder
+		 * @return SchemaScaffolder
+		 */
+		public function provideGraphQLScaffolding(SchemaScaffolder $scaffolder)
+		{
+			// Provide entity type
+			$typeScaffolder = $scaffolder
+				->type('Page')
+				->addFields([
+					'ID',
+					'Title',
+					'Content'
+				]);
+			// Provide operations
+			$typeScaffolder
+				->operation(SchemaScaffolder::READ)
+				->setName('readSiteTrees')
+				->end();
+
+			return $scaffolder;
+		}
+
+		public function canView($member = '')
+		{
+			return true;
+		}
+
+
+	}
+
 }
